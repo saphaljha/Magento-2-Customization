ecc9209dcba8226c5ea7dfff5ad2d0c27dd80f92
saphaljha <saphal.jha@krishtechnolabs.com>
2019-12-26 01:05:19 +0530

    Basic Module files controller with layout

---
+++ app/code/Saphal/MagentoCustomization/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Saphal_MagentoCustomization',
+    __DIR__
+);
